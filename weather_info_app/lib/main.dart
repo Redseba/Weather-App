b12adc293042a2454f6d453b33424f2d5b113b48
Redseba <2004jonathanmorales@gmail.com>
2024-09-25 17:21:12 -0400

    Basic UI with input field and button

--- weather_info_app/lib/main.dart
+++ weather_info_app/lib/main.dart
@@ -33,6 +33,22 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+final TextEditingController _nameController = TextEditingController();
+String cityName = "Your City";
+int Temp = 0;
+String cityStatus = "Rainy";
+
+void _updateCityName() {
+    setState(() {
+      cityName = _nameController.text.isNotEmpty ? _nameController.text : "Your City";
+    });
+  }
+void _fetchWeather() {
+  setState(() {
+    Temp = 15;
+  });
+}
+
   @override
   Widget build(BuildContext context) {
 
@@ -49,7 +65,42 @@ class _MyHomePageState extends State<MyHomePage> {
 
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+
+          TextField(
+            controller: _nameController,
+            decoration: InputDecoration(
+              labelText: 'Enter the name of the city!',
+                border: OutlineInputBorder(),
+            ),
+            onSubmitted: (_) => _updateCityName(),
+            ),
+
+            SizedBox(height: 8.0),
+            Text(
+            'Status: $Temp', // Display the status
+            style: TextStyle(fontSize: 20.0),
+            ),
+
+            SizedBox(height: 8.0),
+            Text(
+            'Status: $cityName', // Display the status
+            style: TextStyle(fontSize: 20.0),
+            ),
+
+            SizedBox(height: 8.0),
+            Text(
+            'Status: $cityStatus', // Display the status
+            style: TextStyle(fontSize: 20.0),
+            ),
+
+            SizedBox(height: 16.0),
+            ElevatedButton(
+            onPressed: _fetchWeather,
+            child: Text('Fetch Weather'),
+            ),
+
             
+
           ],
         ),
       ),
